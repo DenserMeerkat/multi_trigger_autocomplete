28b593c69d0cc3774ff642ab345e0960a2f7153c
DenserMeerkat <mragulmanoharan@gmail.com>
2025-02-13 23:53:38 +0530

    export AutocompleteNoTrigger

--- lib/multi_trigger_autocomplete.dart
+++ lib/multi_trigger_autocomplete.dart
@@ -4,4 +4,5 @@ export 'package:flutter_portal/flutter_portal.dart' show Portal;
 
 export 'src/multi_trigger_autocomplete.dart';
 export 'src/autocomplete_trigger.dart';
+export 'src/autocomplete_no_trigger.dart';
 export 'src/autocomplete_query.dart';
